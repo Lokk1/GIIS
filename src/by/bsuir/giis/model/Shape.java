880fc9f477d403863184c9663bd275693c52aebc
Aleinikov Zhenya <aleinikov.zhenya@gmail.com>
2013-10-04 02:35:53 +0300

    update project

---
+++ src/by/bsuir/giis/model/Shape.java
@@ -0,0 +1,24 @@
+package by.bsuir.giis.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.bsuir.giis.util.Cell;
+
+public class Shape {
+	
+	private List<Cell> shape;
+	
+	public Shape() {
+		setShape(new ArrayList<Cell>());
+	}
+
+	public List<Cell> getShape() {
+		return shape;
+	}
+
+	public void setShape(List<Cell> shape) {
+		this.shape = shape;
+	}
+	
+}
