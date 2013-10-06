2dfcc06bb74f821cf800bec961fd8d5ad9063bd1
Aleinikov Zhenya <aleinikov.zhenya@gmail.com>
2013-10-06 18:23:09 +0300

    new file:   src/by/bsuir/giis/model/ShapeList.java deleted:    src/by/bsuir/giis/model/Shapes.java

---
+++ src/by/bsuir/giis/model/ShapeList.java
@@ -0,0 +1,14 @@
+package by.bsuir.giis.model;
+
+import java.util.List;
+
+public class ShapeList {
+	
+	private List<Shape>  shape;
+	
+	public ShapeList() {
+		// TODO Auto-generated constructor stub
+	
+	}
+
+}
